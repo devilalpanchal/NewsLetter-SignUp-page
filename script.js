ff8f89872d978832a53aaafe82df7a925283dea5
Devilal panchal <devilalpanchal551@gmail.com>
2024-03-11 21:32:51 +0530

    ad

--- script.js
+++ script.js
@@ -1,30 +1,38 @@
-// const subscribeBtn = document.querySelector('.btn');
-// const dismissBtn = document.querySelector('.btn2');
-// const card01 = document.querySelector('.main-container');
-// const successMessage = document.querySelector('.second-Container');
-// const input = document.querySelector('.input');
+const subscribeBtn = document.querySelector('.btn');
+const card01 = document.querySelector('.main-container');
+const successMessage = document.querySelector('.second-Container');
+const dismissBtn = document.querySelector('.btn2');
+const Req = document.querySelector('.Req')
+
+subscribeBtn.addEventListener('click', () => {
+    const input = document.querySelector('.input').value;
+    const FullOfEmail = document.querySelector('#FullOfEmail');
+
+    if (input) {
+        card01.classList.add('hide');
+        successMessage.classList.remove('hide');
+        FullOfEmail.innerText = input;
+    } else {
+        let require = document.createElement('div');
+        require.innerText = 'Valid Email is Required';
+        require.style.color = 'red';
+        Req.appendChild(require);
+    }
+});
 
 
-// subscribeBtn.addEventListener('click', (e) => {
-//     e.preventDefault();
-//     if (input.value.trim() !== '') {
-//         card01.classList.add('hide');
-//         subscribeBtn.style.background = 'black';
-//         successMessage.classList.remove('hide');
-//     } else {
-//         input.style.border = '2px solid red';
-//         input.classList.add('placeholder-red');
-//     }
-// });
+dismissBtn.addEventListener('click', () => {
+    card01.classList.remove('hide')
+    successMessage.classList.add('hide')
+})
 
 
 
-// dismissBtn.addEventListener('click', () => {
-//     card01.classList.remove('hide');
-//     dismissBtn.style.background = 'black'
-//     successMessage.classList.add('hide');
 
-// });
+
+
+
+
 
 
 
